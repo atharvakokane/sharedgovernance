60c02a4d0071f3907b8857f3eedd155e44e54875
Atharva Kokane <atharvashashankkokane@gmail.com>
2026-03-16 00:54:38 -0400

    Revert "Move firebase-config and users to GitHub Secrets; add deploy workflow"
    This reverts commit e71fcc0952e95c93f974cb7879fae9dfe5a66deb.

---
+++ js/firebase-config.js
@@ -0,0 +1,15 @@
+/**
+ * Firebase configuration for shared submissions storage.
+ * Create a project at https://console.firebase.google.com/
+ * Enable Firestore Database, then paste your config below.
+ * Leave as null to use localStorage (submissions stay on each device).
+ */
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAvIKeuLM7140-y3wiKqiz0oVvADpSWQV8",
+  authDomain: "sharedgovernance-52992.firebaseapp.com",
+  projectId: "sharedgovernance-52992",
+  storageBucket: "sharedgovernance-52992.firebasestorage.app",
+  messagingSenderId: "977154498966",
+  appId: "1:977154498966:web:7a8076e46ab19b02447db7",
+  measurementId: "G-34MS20FGJD"
+};
